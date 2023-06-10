9813dd6b50f5d718141bccb2e0b7bc41be0b89ab
Nanami <88866882+krmr73@users.noreply.github.com>
2023-06-10 10:42:04 +0900

    add: 合成データのターゲットデータを生成するスクリプト

---
+++ make_synthetic_target.jl
@@ -0,0 +1,64 @@
+using PolyaUrnSimulator
+using StatsBase
+using ProgressMeter
+
+include("lib/history2vec.jl")
+
+function run_existing_model(rho::Int, nu::Int, s::String)
+  _s::Union{Function,Nothing} = nothing
+  if s == "SSW"
+    _s = ssw_strategy!
+  elseif s == "WSW"
+    _s = wsw_strategy!
+  else
+    throw(error("strategy must be SSW or WSW"))
+  end
+
+  env = Environment()
+  init_agents = [
+    Agent(rho, nu, _s)
+    Agent(rho, nu, _s)
+  ]
+  init!(env, init_agents)
+
+
+  for _ in 1:20000
+    step!(env)
+  end
+
+  return history2vec(env.history, 1000)
+end
+
+function synthetic_target()
+  outdir = mkpath("data")
+  outfile = "$outdir/synthetic_fitting_target.csv"
+
+  open(outfile, "w") do fp
+    println(fp, "rho,nu,s,gamma,no,nc,oo,oc,c,y,g,r,h")
+  end
+
+  N = 1:10
+  rhos = [1, 10, 20]
+  nus = [1, 10, 20]
+  ss = ["SSW", "WSW"]
+
+  p = Progress(length(N) * length(rhos) * length(nus) * length(ss))
+  lk = ReentrantLock()
+  Threads.@threads for _ in N
+    Threads.@threads for rho in rhos
+      Threads.@threads for nu in nus
+        Threads.@threads for s in ss
+          res = run_existing_model(rho, nu, s)
+          lock(lk) do
+            open(outfile, "a") do fp
+              println(fp, join(string.(values((; rho, nu, s, res...))), ","))
+            end
+          end
+          next!(p)
+        end
+      end
+    end
+  end
+end
+
+synthetic_target()
