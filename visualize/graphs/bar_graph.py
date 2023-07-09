15149820decd784e30bc96e9bcc0c2182a2f95e6
Nanami <88866882+krmr73@users.noreply.github.com>
2023-07-09 16:01:45 +0900

    refactor: algorithmごとの処理をまとめる

--- visualize/graphs/bar_graph.py
+++ visualize/graphs/bar_graph.py
@@ -21,35 +21,30 @@ def plot_bar_graph(target_type: str, targets: list, my_color: dict) -> None:
     os.makedirs("results/bar_graph", exist_ok=True)
 
     if target_type == "empirical":
+        df = pd.DataFrame(columns=["distance", "target", "model"])
+
         for target in targets:
             emp = pd.read_csv(f"../data/{target}.csv").iloc[0]
 
-            fs_best_params = (fs_results_mean - emp).abs().sum(axis=1).idxmin()
-            fs_best_vecs = fs_results.loc[fs_best_params, :]
-            fs_best_diffs = (fs_best_vecs - emp).abs().sum(axis=1)
-            fs_df = pd.DataFrame(data=fs_best_diffs, columns=["distance"])
-            fs_df["target"] = target_labels[target]
-            fs_df["model"] = "Existing"
+            algorithm_labels = [
+                {"algorithm": "full-search", "model": "Existing"},
+                {"algorithm": "qd", "model": "Proposed"},
+                {"algorithm": "ga", "model": "GA"},
+                {"algorithm": "random-search", "model": "Random Search"},
+            ]
 
-            qd_best_vecs = pd.read_csv(f"results/fitted/{target}/qd.csv")
-            qd_best_diffs = (qd_best_vecs - emp).abs().sum(axis=1)
-            qd_df = pd.DataFrame(data=qd_best_diffs, columns=["distance"])
-            qd_df["target"] = target_labels[target]
-            qd_df["model"] = "Proposed"
+            for label in algorithm_labels:
+                if label["algorithm"] == "full-search":
+                    best_params = (fs_results_mean - emp).abs().sum(axis=1).idxmin()
+                    best_vecs = fs_results.loc[best_params, :]
+                else:
+                    best_vecs = pd.read_csv(f"results/fitted/{target}/{label['algorithm']}.csv")
+                best_diffs = (best_vecs - emp).abs().sum(axis=1)
+                algorithm_df = pd.DataFrame(data=best_diffs, columns=["distance"])
+                algorithm_df["target"] = target_labels[target]
+                algorithm_df["model"] = label["model"]
 
-            ga_best_vecs = pd.read_csv(f"results/fitted/{target}/ga.csv")
-            ga_best_diffs = (ga_best_vecs - emp).abs().sum(axis=1)
-            ga_df = pd.DataFrame(data=ga_best_diffs, columns=["distance"])
-            ga_df["target"] = target_labels[target]
-            ga_df["model"] = "GA"
-
-            rs_best_vecs = pd.read_csv(f"results/fitted/{target}/random-search.csv")
-            rs_best_diffs = (rs_best_vecs - emp).abs().sum(axis=1)
-            rs_df = pd.DataFrame(data=rs_best_diffs, columns=["distance"])
-            rs_df["target"] = target_labels[target]
-            rs_df["model"] = "Random Search"
-
-            df = pd.concat([df, fs_df, qd_df, ga_df, rs_df], ignore_index=True)
+                df = pd.concat([df, algorithm_df], ignore_index=True)
 
         g: sns.axisgrid.FacetGrid = sns.catplot(
             height=4,
