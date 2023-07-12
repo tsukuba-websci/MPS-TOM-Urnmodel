97e38c05f7cc776f4f3d1b76b27916e9c674aa42
krmr73 <makoto0117hanayo@gmail.com>
2023-07-12 13:25:21 +0000

    add: 遺伝子型のQDマップも追加

--- visualize/graphs/qd_map.py
+++ visualize/graphs/qd_map.py
@@ -8,12 +8,29 @@ from ribs.archives import CVTArchive
 from sklearn.manifold import TSNE
 
 
-def phenotype_map(df: pd.DataFrame, target: str, cmap: LinearSegmentedColormap) -> None:
-    # graphのベクトルの抽出
-    dim = df.filter(regex="measure_\d+").shape[1]
-    column_names = [f"measure_{i}" for i in range(dim)]
-    data = df[column_names]
+def plot(reduced_data_df: pd.DataFrame, cmap: LinearSegmentedColormap, dir: str, target: str) -> None:
+    os.makedirs(dir, exist_ok=True)
+    plt.rcParams["font.size"] = 30
+    plt.figure(figsize=(10, 8))
+    scatter = plt.scatter(
+        reduced_data_df["t-sne1"],
+        reduced_data_df["t-sne2"],
+        c=reduced_data_df["distance"],
+        cmap=cmap,
+        s=15,
+    )
+    cbar = plt.colorbar(scatter, label="d", orientation="vertical", shrink=1)
+    cbar.ax.invert_yaxis()
+    cbar.ax.tick_params(labelsize=22)
+    plt.xlabel("t-SNE 1")
+    plt.ylabel("t-SNE 2")
+    plt.tick_params(axis="both", labelsize=22)
+    plt.tight_layout()
+    plt.savefig(f"{dir}/{target}.png", dpi=200)
+    plt.close()
 
+
+def reduce_dimension(data: pd.DataFrame) -> pd.DataFrame:
     # 正規化
     normalized_data = (data - data.mean()) / data.std()
 
@@ -24,28 +41,41 @@ def phenotype_map(df: pd.DataFrame, target: str, cmap: LinearSegmentedColormap) 
     # reduced_dataをDataFrameに変換
     reduced_data_df = pd.DataFrame(reduced_data, columns=["t-sne1", "t-sne2"])
 
+    return reduced_data_df
+
+
+def phenotype_map(df: pd.DataFrame, target: str, cmap: LinearSegmentedColormap) -> None:
+    # graphのベクトルの抽出
+    dim = df.filter(regex="measure_\d+").shape[1]
+    column_names = [f"measure_{i}" for i in range(dim)]
+    data = df[column_names]
+
+    # 次元削減
+    reduced_data_df = reduce_dimension(data)
+
     # "distance"列を追加
     reduced_data_df["distance"] = df["objective"].abs()
 
     # 可視化
-    os.makedirs("results/qd_map", exist_ok=True)
-    plt.rcParams["font.size"] = 22
-    plt.figure(figsize=(12, 10))
-    scatter = plt.scatter(
-        reduced_data_df["t-sne1"],
-        reduced_data_df["t-sne2"],
-        c=reduced_data_df["distance"],
-        cmap=cmap,
+    plot(reduced_data_df, cmap, "results/qd_map/phenotype", target)
+
+
+def genotype_map(df: pd.DataFrame, target: str, cmap: LinearSegmentedColormap) -> None:
+    # 遺伝子の抽出
+    df.rename(
+        columns={"solution_0": "rho", "solution_1": "nu", "solution_2": "recentness", "solution_3": "frequency"},
+        inplace=True,
     )
-    cbar = plt.colorbar(scatter, label="d", orientation="vertical", shrink=1)
-    cbar.ax.invert_yaxis()
-    cbar.ax.tick_params(labelsize=16)
-    plt.xlabel("t-SNE 1")
-    plt.ylabel("t-SNE 2")
-    plt.tick_params(axis="both", labelsize=18)
-    plt.tight_layout()
-    plt.savefig(f"results/qd_map/{target}.png", dpi=300)
-    plt.close()
+    data = df[["rho", "nu", "recentness", "frequency"]]
+
+    # 次元削減
+    reduced_data_df = reduce_dimension(data)
+
+    # "distance"列を追加
+    reduced_data_df["distance"] = df["objective"].abs()
+
+    # 可視化
+    plot(reduced_data_df, cmap, "results/qd_map/genotype", target)
 
 
 def plot_qd_map(target_type: str, targets: list, my_color: dict) -> None:
@@ -61,6 +91,6 @@ def plot_qd_map(target_type: str, targets: list, my_color: dict) -> None:
         with open(f"../qd/results/{target}/archive.pkl", "rb") as f:
             archive: CVTArchive = pickle.load(f)
         df = archive.as_pandas()
-        print(df)
 
         phenotype_map(df, target, my_green_to_red_cmap)
+        genotype_map(df, target, my_green_to_red_cmap)
