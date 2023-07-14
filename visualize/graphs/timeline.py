7c034d17bf2f0fdb83ad2f7da89e3d39623183aa
Nanami <88866882+krmr73@users.noreply.github.com>
2023-07-14 09:45:10 +0900

    fix: フォントサイズと色を修正

--- visualize/graphs/timeline.py
+++ visualize/graphs/timeline.py
@@ -4,7 +4,7 @@ import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 
-plt.rcParams["font.size"] = 16
+plt.rcParams["font.size"] = 18
 
 
 def archives2df(df: pd.DataFrame, df_min: pd.DataFrame, target: str, algorithm: str):
@@ -22,62 +22,75 @@ def archives2df(df: pd.DataFrame, df_min: pd.DataFrame, target: str, algorithm: 
     return df, df_min
 
 
-def plot(df: pd.DataFrame, df_min: pd.DataFrame):
-    fig, ax = plt.subplots(figsize=(8, 5))
-    sns.lineplot(
-        data=df_min,
-        x="generation",
-        y="distance",
-        hue="target",
-        legend=False,
-        linestyle="--",
-        ax=ax,
-    )
-    sns.lineplot(
-        data=df,
-        x="generation",
-        y="distance",
-        hue="target",
-        legend=False,
-        ax=ax,
-        alpha=0.3,
-    )
-
-
 def plot_timeline(target_type: str, targets: list, my_color: dict) -> None:
     algorithms = ["ga", "qd"]
 
     for algorithm in algorithms:
         # 実データに対しては、全てのターゲットをまとめてプロット
         if target_type == "empirical":
-
             os.makedirs(f"results/timeline/{algorithm}", exist_ok=True)
 
             df = pd.DataFrame()
             df_min = pd.DataFrame()
             for target in targets:
                 df, df_min = archives2df(df, df_min, target, algorithm)
-            plot(df, df_min)
-
-            plt.xlabel("Generation")
-            plt.ylabel("d")
+            fig, ax = plt.subplots(figsize=(8, 5))
+            sns.lineplot(
+                data=df_min,
+                x="generation",
+                y="distance",
+                hue="target",
+                legend=False,
+                linestyle="--",
+                ax=ax,
+            )
+            sns.lineplot(
+                data=df,
+                x="generation",
+                y="distance",
+                hue="target",
+                legend=False,
+                ax=ax,
+                alpha=0.3,
+            )
+            plt.xlabel("Generation", fontsize=24)
+            plt.ylabel("d", fontsize=24)
             plt.tight_layout()
             plt.savefig(f"results/timeline/{algorithm}/{target_type}.png", dpi=300)
             plt.close()
 
         # 合成データに対しては、ターゲットごとにプロット
         else:
-            color_order = [my_color["dark_red"]]
             os.makedirs(f"results/timeline/{algorithm}/synthetic", exist_ok=True)
 
             for target in targets:
                 df = pd.DataFrame()
                 df_min = pd.DataFrame()
                 df, df_min = archives2df(df, df_min, target, algorithm)
-                plot(df, df_min, color_order)
+                fig, ax = plt.subplots(figsize=(8, 5))
+                sns.lineplot(
+                    data=df_min,
+                    x="generation",
+                    y="distance",
+                    hue="target",
+                    legend=False,
+                    linestyle="--",
+                    palette=[my_color["dark_red"]],
+                    ax=ax,
+                )
+                sns.lineplot(
+                    data=df,
+                    x="generation",
+                    y="distance",
+                    hue="target",
+                    legend=False,
+                    ax=ax,
+                    palette=[my_color["dark_red"]],
+                    alpha=0.3,
+                )
 
-                plt.xlabel("Generation")
-                plt.ylabel("d")
+                plt.xlabel("Generation", fontsize=24)
+                plt.ylabel("d", fontsize=24)
                 plt.tight_layout()
                 plt.savefig(f"results/timeline/{algorithm}/{target}.png", dpi=300)
                 plt.close()
